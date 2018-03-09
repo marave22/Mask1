af6e1d2d5d268b66d094d47c8021c45892c075fe
marave22 <36631465+marave22@users.noreply.github.com>
2018-03-09 18:26:03 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-'use strict'
 var slideIndex = 0;
 showSlides();
 
 function showSlides() {
     var i;
-    var mySlides = document.getElementsByClassName("slides");
+    var mySlides = document.getElementsByClassName("slide_content");
     var dots = document.getElementsByClassName("dot");
     for (i = 0; i < mySlides.length; i++) {
         mySlides[i].style.display = "none";
@@ -17,6 +16,5 @@ function showSlides() {
         dots[i].className = dots[i].className.replace(" active", "");
     }
     mySlides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
     setTimeout(showSlides, 2000);
 }
