32da3852d6607665cd925cda6f35ac64dd9b1401
marave22 <36631465+marave22@users.noreply.github.com>
2018-03-13 02:13:43 +0400

    Delete grid.js

--- grid.js
+++
@@ -1,8 +0,0 @@
-var elements = document.getElementsByClassName("column");
-var i;
-function two() {
-    for (i = 0; i < elements.length; i++) {
-        elements[i].style.msFlex = "50%";  // IE10
-        elements[i].style.flex = "50%";
-    }
-}
