4c5920e38c03e109cc96a7c30c1b5dae38f0e8a6
marave22 <36631465+marave22@users.noreply.github.com>
2018-03-19 17:55:41 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -18,3 +18,18 @@ function showSlides() {
     mySlides[slideIndex - 1].style.display = "block";
     setTimeout(showSlides, 2000);
 }
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
